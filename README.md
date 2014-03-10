7d042de5bcb54dc3e496994689c37f31c5f91762
axfelix <axfelix@gmail.com>
2014-03-10 16:28:03 -0700

    got rid of test credential prompts; began adding db stuff

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Files related to the Public Knowlege Project Private LOCKSS Network.
+
+requires: lxml, mysql-python, web.py
---
+++ config.py
@@ -0,0 +1,4 @@
+LOCAL_DB_HOST="localhost"
+LOCAL_DB_PORT=3306
+LOCAL_DB_USER="swordsman"
+LOCAL_DB_PASSWORD="hesgotanarmoff"
