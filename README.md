8393d1941f0de052b42cce55b1d9323ab6cf7e27
Mark Jordan <mjordan@sfu.ca>
2014-03-17 21:03:14 -0700

    Removed Simple-Sword-Server submodule.

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 Files related to the Public Knowlege Project Private LOCKSS Network.
 
-requires: lxml, mysql-python, web.py
--- config.py
+++
@@ -1,4 +0,0 @@
-LOCAL_DB_HOST="localhost"
-LOCAL_DB_PORT=3306
-LOCAL_DB_USER="swordsman"
-LOCAL_DB_PASSWORD="hesgotanarmoff"
