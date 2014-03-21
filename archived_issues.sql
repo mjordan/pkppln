27cad9850fa3d1e498b572b246380a349a8c2f97
Mark Jordan <mjordan@sfu.ca>
2014-03-21 13:30:14 -0700

    Update archived_issues database table.

--- archived_issues.sql
+++ archived_issues.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 18, 2014 at 03:28 PM
+-- Generation Time: Mar 21, 2014 at 01:29 PM
 -- Server version: 5.5.35-0ubuntu0.13.10.2
 -- PHP Version: 5.5.3-1ubuntu2.2
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `archived_issues` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `action` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
-  `journal_manager_email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `contact_email` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
   `deposit_uuid` varchar(38) COLLATE utf8_unicode_ci NOT NULL,
   `date_deposited` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `journal_uuid` varchar(38) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -40,7 +40,14 @@ CREATE TABLE IF NOT EXISTS `archived_issues` (
   `harvested` timestamp NULL DEFAULT NULL,
   `deposited_lom` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=40 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=56 ;
+
+--
+-- Dumping data for table `archived_issues`
+--
+
+INSERT INTO `archived_issues` (`id`, `action`, `contact_email`, `deposit_uuid`, `date_deposited`, `journal_uuid`, `sha1_value`, `issue_url`, `size`, `status`, `harvested`, `deposited_lom`) VALUES
+(55, 'add', 'journal_manager@example.com', '1225c695-cfb8-4ebb-aaaa-80da344efa6a', '2014-03-19 04:13:42', 'a120bcd6-3204-4c65-b454-6effd76a2bed', 'bd4a9b642562547754086de2dab26b7d', 'http://jfs.example.org/download/file1.zip', 102400, 'deposited', NULL, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
