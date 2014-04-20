83d4de5a12d7b99456dffe1a07ee9bcf58611631
Mark Jordan <mjordan@sfu.ca>
2014-04-19 18:13:47 -0700

    Work on reserialize_bag microservice.

--- services/reserialize_bag.py
+++ services/reserialize_bag.py
@@ -6,7 +6,10 @@ journals participating in the PKP PLN.
 import os
 import ConfigParser
 import bagit
-import zipfile
+import tarfile
+import shutil
+import hashlib
+import string
 from datetime import datetime
 
 import staging_server_common
@@ -19,25 +22,66 @@ config.read('../config_dev.cfg')
 microservice_name = 'reserialize_deposit_bags'
 microservice_state = 'reserialized'
 # The name of the directory under the processing root directory. One of
-# 'havested', 'unserialized', or 'reserialized'.
-input_directory = 'unserialized'
+# 'havested', 'bagValidated', or 'reserialized'.
+input_directory = 'bagValidated'
 
 def reserialize_bag(deposit):
     started_on = datetime.now()
     deposit_uuid = deposit['deposit_uuid']
     outcome = 'success'
     error = ''
-    # @todo: 1) Add these tags to bag-info.txt: Bagging-Date, External-Description [value of the journal title and issue number],
+    
+    # Read the source Bag's manifest-md5.txt file and read it into memory
+    # so we can verify that the files have copied over intact.
+    path_to_source_md5_manifest = staging_server_common.get_input_path(input_directory, [deposit_uuid, deposit_uuid, 'manifest-md5.txt'])
+    md5s = {}
+    with open(path_to_source_md5_manifest) as md5f:
+        for line in md5f:
+            (md5, path) = line.split()
+            # Remove 'data/' from the file's name.
+            clean_path = string.replace(path, 'data/', '')
+            print "Path is now " + clean_path
+            md5s[clean_path] = md5
+
+    # Copy the files from the 'bagValidate' data directory into a
+    # temporary directory, where the new Bag will be created
+    path_to_source_files = staging_server_common.get_input_path(input_directory, [deposit_uuid, deposit_uuid, 'data'])
+    path_to_temp_directory = staging_server_common.create_microservice_directory(microservice_state, deposit_uuid)
+    print path_to_temp_directory
+    source_files = os.listdir(path_to_source_files)
+    for source_file in source_files:
+        # 'virus_check.txt' was not in the original Bag so we skip it.
+        if source_file != 'virus_check.txt':
+            shutil.copy(os.path.join(path_to_source_files, source_file), path_to_temp_directory)
+            path_to_copied_file = os.path.join(path_to_temp_directory, source_file)
+            copied_file = open(path_to_copied_file, 'rb')
+            md5_value = hashlib.md5()
+            while True:
+                # Read the file in 10 mb chunks so we don't run out of memory.
+                buf = copied_file.read(10 * 1024 * 1024)
+                if not buf:
+                    break
+                md5_value.update(buf)
+            copied_file.close()
+            if md5s[source_file] != md5_value.hexdigest():
+                raise Exception("md5 checksums don't match for %s" % (path_to_copied_file))
+    
+    
+    # Add these tags to bag-info.txt: Bagging-Date, External-Description [value of the journal title and issue number],
     # External-Identifier [value of the deposit URL], PKP-PLN-Journal-UUID, PKP-PLN-Deposit-UUID.
-    # 2) Add the virus report to the Bag; 3) Reserialize the Bag and assign it a filename using the pattern
-    # journaluuid.issueuuid.zip (max 255 chars including extension).
-    
-    # @todo: add the virus report to the Bag
-    # path_to_virus_check_report = path = os.path.join(config.get('Paths', 'processing_root'), input_directory, deposit_uuid, 'virus_check.txt')    
-    
-    # @todo: Recreate Bag and serialize it (using .zip) with the deposit filename.
-    path_to_unserialized_deposit = staging_server_common.get_input_path(input_directory, deposit_uuid)
-    # deposit_filename = staging_server_common.get_deposit_filename(deposit_url)
+    # @todo: Get journal title and issue number.
+    tags = {
+        'External-Identifier': deposit['deposit_url'],
+        'PKP-PLN-Journal-UUID': deposit['journal_uuid'],
+        'PKP-PLN-Deposit-UUID': deposit['deposit_uuid']
+    }
+    bag = bagit.make_bag(path_to_temp_directory, tags)
+
+    # Reserialize the Bag and assign it a filename using the pattern journaluuid.deposituuid.zip.
+    serialized_bag_filename = '.'.join([deposit['journal_uuid'], deposit['deposit_uuid'], 'tar.gz'])
+    serialzied_bag_path = os.path.join(config.get('Paths', 'processing_root'), microservice_state, serialized_bag_filename)
+    with tarfile.open(serialzied_bag_path, "w:gz") as tar:
+        tar.add(path_to_temp_directory, arcname=os.path.basename(path_to_temp_directory))
     
     finished_on = datetime.now()
 
