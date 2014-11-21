b33f192cacd95b0a9b0fa40d214c3b73607d39d8
Michael Joyce <ubermichael@gmail.com>
2014-11-20 16:09:16 -0800

    document the tests, and fix an import.

--- test/test_feeds.py
+++ test/test_feeds.py
@@ -4,25 +4,42 @@ import unittest
 import sys
 import xml.etree.ElementTree as ET
 import json
-sys.path.append("/opt/pkppln")
+from os.path import abspath, dirname
+sys.path.append(dirname(dirname(abspath(__file__))))
 from feeds.feed_server import terms_feed, get_config, get_connection
 import MySQLdb.cursors
 
 
 class TestFeeds(unittest.TestCase):
 
+    """
+    Class to test the feeds generator. Doesn't attempt to test the WSGI
+    stuff at all, as that's just a thin wrapper around feeds_server.py
+    """
+
     def test_get_config(self):
+        """
+        Get a config object, and check that a required
+        field is present.
+        """
         config = get_config()
         try:
             config.get('Database', 'db_host')
-        except:
+        except Exception:
             self.fail('Cannot find Database.db_host config.')
 
     def test_get_connection(self):
+        """
+        Get a database cursor.
+        """
         connection = get_connection()
         self.assertIsInstance(connection, MySQLdb.cursors.DictCursor)
 
     def test_terms_rss(self):
+        """
+        Generate an RSS feed. Verifies that the feed contains a known
+        string, and that it has more than zero item elements.
+        """
         content = str(terms_feed('rss'))
         self.assertTrue('PKP PLN Terms' in content)
         root = ET.fromstring(content)
@@ -30,6 +47,10 @@ class TestFeeds(unittest.TestCase):
         self.assertGreater(len(root.findall('.//item')), 0)
 
     def test_terms_atom(self):
+        """
+        Generate an Atom Feed. Verifies that the feed contains a
+        known string, and that it has more than zero entry elements.
+        """
         content = str(terms_feed('atom'))
         self.assertTrue('PKP PLN Terms' in content)
         root = ET.fromstring(content)
@@ -41,12 +62,18 @@ class TestFeeds(unittest.TestCase):
         )
 
     def test_terms_json(self):
+        """
+        Generate a JSON feed.
+        """
         content = str(terms_feed('json'))
         self.assertGreater(len(content), 0)
         root = json.loads(content)
         self.assertGreater(len(root), 0)
 
     def test_terms_error(self):
+        """
+        Attempt to generate an invalid feed.
+        """
         response = terms_feed('fooooooo')
         self.assertEquals('404 Not Found', response.status)
         self.assertEquals('', str(response))
